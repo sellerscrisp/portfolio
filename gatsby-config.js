3b45eb14e88d9a4fa1752c813c03b34b09a229cc
sellerscrisp <sellershcrisp@gmail.com>
2021-04-27 10:45:21 -0400

    Removed algoliasearch before deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,17 +90,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-algolia-search`,
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_ADMIN_KEY,
-        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
-        queries,
-        chunkSize: 10000, // default: 1000
-        enablePartialUpdates: true,
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Sellers Crisp`,
